a68207acc19df9a4b20db868c65676feb54e4be8
Chetan <chetan@swordfishtech.co.uk>
2024-04-30 10:01:09 +0530

    First commit with initial API setup

---
+++ books.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+
+
+
+app = FastAPI()
+
+BOOKS = {
+    'book_1':{'title': 'On the Road', 'author': 'Jack Kerouac'},
+    'book_2':{'title': 'Title Two', 'author': 'Author Two'},
+    'book_3':{'title': 'Title Three', 'author': 'Author Three'},
+    'book_4':{'title': 'Title Four', 'author': 'Author Four'},
+    'book_5':{'title': 'Title Five', 'author': 'Author Five'},
+    
+}
+
+
+@app.get("/")
+async def read_all_books():
+    return BOOKS
